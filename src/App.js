d47475091e216405df857023679287f108feacae
Luna Goodnight <catsheue@gmail.com>
2019-11-28 19:41:09 +0800

    back home :beers:

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Front End Developer</p>
-        <ul>
-          <li>abd</li>
-        </ul>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// http://my-json-server.typicode.com/catsheue/lunalinks/links/1
+
+
+
+class App extends Component {
+	constructor(props){
+    super(props);
+    this.state = {
+        links: {}
+    };
+	}
+	componentDidMount() {
+    this.handleSearch();
+	}
+
+    handleSearch = () =>{
+        let url = 'http://my-json-server.typicode.com/catsheue/lunalinks/links';
+        fetch(url).
+        then(response => response.json()).then((links) => {
+            console.log(links);
+            this.setState({
+                links: links
+            });
+        });
+    };
+    render () {
+        const { links } = this.state;
+        debugger
+        return (
+            <div className="App app">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Front End Developer</p>
+                    <ul>
+                        <li>abd</li>
+                    </ul>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
