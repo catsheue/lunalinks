d0dd301864d948c56ab86955e0d6dfa0bbd5caae
Luna Goodnight <catsheue@gmail.com>
2019-12-15 04:04:50 +0800

    change to use local link file :fire:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
-import linksObj from "./assets/db";
+import linksObj from "./assets/db.js";
 // http://my-json-server.typicode.com/catsheue/lunalinks/links/1
-
 
 
 class App extends Component {
@@ -13,10 +12,12 @@ class App extends Component {
     };
 	}
 	componentDidMount() {
-    this.handleSearch();
+    this.renderLinks();
 	}
 
 	renderLinks = () => {
+
+    const links = linksObj;
 		const add = (arr, name, source) => {
 			const found = arr.some(el => el.type === name);
 			if (!found) {
@@ -31,7 +32,7 @@ class App extends Component {
 
 			return arr;
 		}
-		const sortlinks = linksObj.reduce((a, c) => {
+		const sortlinks = links.reduce((a, c) => {
 			add(a, c.type, c);
 			return a;
 		}, [])
@@ -51,47 +52,6 @@ class App extends Component {
 		});
 	}
 
-  handleSearch = () =>{
-    // let url = 'http://my-json-server.typicode.com/catsheue/lunalinks/links?a=1';
-    let url = 'http://localhost:3000/links/';
-	  // json-server --watch db.json
-    fetch(url).then(response => response.json()).then((links) => {
-
-
-      const add = (arr, name, source) => {
-        const found = arr.some(el => el.type === name);
-        if (!found) {
-          arr.push({ type: name, item: []})
-          const index = arr.findIndex(fruit => fruit.type === name);
-          arr[index].item.push({...source});
-
-        } else{
-          const index = arr.findIndex(fruit => fruit.type === name);
-          arr[index].item.push({...source});
-        }
-
-        return arr;
-      }
-      const sortlinks = links.reduce((a, c) => {
-        add(a, c.type, c);
-        return a;
-      }, [])
-
-      let x;
-
-      const sortOrder = (a, b) => {
-	     const aOrder = a.order ? a.order : -1;
-	     const bOrder = b.order ? b.order : -1;
-	     return bOrder - aOrder;
-      }
-      for (x of sortlinks) {
-	      x.item.sort(sortOrder);
-      }
-      this.setState({
-          links: sortlinks
-      });
-    });
-  };
   render () {
     const { links } = this.state;
     return (
---
+++ src/assets/db.js
@@ -0,0 +1,359 @@
+const obj =  [
+      {
+        "type": "Front End Developer",
+        "title": "A Drip of JavaScript",
+        "links": "http://adripofjavascript.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Latest ReactJS Examples",
+        "links": "https://react.rocks/"
+      },
+      {
+        "type": "Jobs",
+        "title": "Find front-end developer jobs (remote & on site) & learn to be a front-end developer",
+        "links": "http://frontenddeveloperjob.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Git Tutorial | Backlog",
+        "links": "https://backlog.com/git-tutorial/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Smashing Magazine",
+        "links": "https://backlog.com/git-tutorial/",
+        "description": "For Web Designers And Developers"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Smtebooks",
+        "links": "https://smtebooks.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Buy Plugins & Code from CodeCanyon",
+        "links": "https://codecanyon.net/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Buy Apps & Games Source Code iOS / Android - Sell My App",
+        "links": "https://www.sellmyapp.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Learn with Param",
+        "links": "https://learnwithparam.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "webSemantics",
+        "links": "https://websemantics.uk/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Buy Code, Scripts, Themes, Plugins and more - Codester",
+        "links": "https://www.codester.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Sellcodes - Buy & Sell Codes",
+        "links": "https://sellcodes.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Gitstore » Get paid for your work",
+        "links": "https://gitstore.app/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "GitMarket :: The right way to sell code",
+        "links": "https://git.market/"
+      },
+      {
+        "type": "Funny",
+        "title": "BU UP -Beauty Upgrade",
+        "links": "https://beauty-upgrade.tw/"
+      },
+      {
+        "type": "Funny",
+        "title": "Twitter",
+        "links": "https://twitter.com/"
+      },
+      {
+        "type": "Funny",
+        "title": "笑话网－段子",
+        "links": "https://www.xiaohua.com/",
+        "description": "冷笑话，搞笑图片，逗趣视频，成人笑话，在线分享社区"
+      },
+      {
+        "type": "Funny",
+        "title": "哆啦A夢中文網",
+        "links": "https://chinesedora.com/"
+      },
+      {
+        "type": "Funny",
+        "title": "LoveToKnow: Advice you can trust",
+        "links": "https://www.lovetoknow.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Nordic APIs",
+        "links": "https://nordicapis.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "freeCodeCamp.org",
+        "links": "https://www.freecodecamp.org/news"
+      },
+      {
+        "type": "Game",
+        "title": "巴哈姆特電玩資訊站",
+        "links": "https://www.gamer.com.tw/"
+      },
+      {
+        "type": "Game",
+        "title": "Welcome to Steam",
+        "links": "https://store.steampowered.com/"
+      },
+      {
+        "type": "Game",
+        "title": "ファミ通.com",
+        "links": "https://www.famitsu.com/",
+        "description": "ゲーム・エンタメ最新情報"
+      },
+      {
+        "type": "Game",
+        "title": "Gamer Guides",
+        "links": "https://www.gamerguides.com/"
+      },
+      {
+        "type": "Bet",
+        "title": "奖多多",
+        "links": "https://www.jiangduoduo.com/lottery/#/home/index"
+      },
+      {
+        "type": "Game",
+        "title": "Twinfinite",
+        "links": "https://twinfinite.net/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Overflowjs",
+        "links": "https://overflowjs.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Andreas Reiterer",
+        "links": "https://www.andreasreiterer.at/"
+      },
+      {
+        "type": "Tools for Web Developers",
+        "title": "Carbon",
+        "links": "https://carbon.now.sh/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Overreacted — A blog by Dan Abramov",
+        "links": "https://overreacted.io/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "张鑫旭-鑫空间-鑫生活",
+        "links": "https://www.zhangxinxu.com/wordpress/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Medium",
+        "links": "https://medium.com/",
+        "description": "Get smarter about what matters to you.",
+        "order": 2
+      },
+      {
+        "type": "Front End Developer",
+        "title": "clubmate.fi",
+        "links": "https://clubmate.fi/",
+        "description": "About frontend web development"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "The Vanilla JS Toolkit",
+        "links": "https://vanillajstoolkit.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "mbeaudru/modern-js-cheatsheet",
+        "links": "https://github.com/mbeaudru/modern-js-cheatsheet"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Xebia Blog",
+        "links": "http://blog.xebia.in/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "JavaScript | @tutorialhorizon",
+        "links": "https://javascript.tutorialhorizon.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Codecondo",
+        "links": "https://codecondo.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "The Modern JavaScript Tutorial",
+        "links": "https://javascript.info/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "LeetCode",
+        "links": "https://leetcode.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Udemy",
+        "order": 1,
+        "links": "https://www.udemy.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "Regex Crossword",
+        "links": "https://regexcrossword.com/"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "CodersHood",
+        "links": "https://www.codershood.info/",
+        "description": "Web developement Tutorials, Articles and Free Ebooks"
+      },
+      {
+        "type": "Front End Developer",
+        "title": "@nicoespeon's blog",
+        "links": "https://www.nicoespeon.com/"
+      },
+      {
+        "type": "Funny",
+        "title": "IDEAS.TED.COM",
+        "links": "https://ideas.ted.com/"
+      },
+      {
+        "type": "Funny",
+        "title": "PR TIMES",
+        "links": "https://prtimes.jp/",
+        "description": "プレスリリース・ニュースリリース配信サービス シェアNo.1"
+      },
+      {
+        "type": "Funny",
+        "title": "BeautiMode 創意生活風格網",
+        "links": "https://www.beautimode.com/"
+      },
+      {
+        "type": "Shop",
+        "title": "Fred",
+        "links": "Fun & Whimsical Products - Official Site - GenuineFred.com",
+        "description": "Fun & Whimsical Products - Official Site - GenuineFred.com"
+      },
+      {
+        "type": "Funny",
+        "title": "MOSHI MOSHI NIPPON",
+        "links": "https://www.moshimoshi-nippon.jp/zh-hant/",
+        "description": "もしもしにっぽん | We deliver the latest news on Japanese pop culture in English before anyone else!"
+      },
+      {
+        "type": "Funny",
+        "title": "新假期周刊",
+        "links": "https://www.weekendhk.com"
+      },
+      {
+        "type": "Funny",
+        "title": "Bored Panda",
+        "links": "https://www.boredpanda.com/"
+      },
+      {
+        "type": "Anime",
+        "title": "WebNewtype",
+        "links": "https://webnewtype.com/"
+      },
+      {
+        "type": "Anime",
+        "title": "Anime News Network",
+        "links": "https://www.animenewsnetwork.com/"
+      },
+      {
+        "type": "Anime",
+        "title": "アニメハック",
+        "links": "https://webnewtype.com/",
+        "description": "最新アニメ・イベント情報、番組表まで！"
+      },
+      {
+        "type": "Anime",
+        "title": "カドカワストア",
+        "links": "https://store.kadokawa.co.jp/shop/",
+        "description": "KADOKAWA公式オンラインショップ"
+      },
+      {
+        "type": "Beauty",
+        "title": "尚之潮网",
+        "links": "https://www.shangc.net/",
+        "description": " 前沿时尚生活资讯 娱乐明星八卦最新动态"
+      },
+      {
+        "type": "Otome",
+        "title": "にじめん",
+        "links": "https://nijimen.net/"
+      },
+      {
+        "type": "IT",
+        "title": "JavaWorld.com",
+        "links": "https://www.javaworld.com/"
+      },
+      {
+        "type": "IT",
+        "title": "The Tech Terms Computer Dictionary",
+        "links": "https://techterms.com/"
+      },
+      {
+        "type": "IT",
+        "title": "Webopedia: Online Tech Dictionary for IT Professionals",
+        "links": "https://www.webopedia.com/"
+      },
+      {
+        "type": "Tool",
+        "title": "Business Name Generator & Domain Name Search",
+        "links": "https://www.panabee.com/"
+      },
+      {
+        "type": "Tool",
+        "title": "YourDictionary",
+        "links": "https://www.yourdictionary.com/"
+      },
+      {
+        "type": "Farm",
+        "title": "GigCasa 激趣網",
+        "links": "https://www.gigcasa.com/"
+      },
+      {
+        "type": "Funny",
+        "title": "食力foodNEXT‧食事求實的知識頻道",
+        "links": "https://www.foodnext.net/"
+      },
+      {
+        "type": "Funny",
+        "title": "Zi 字媒體",
+        "links": "https://zi.media/"
+      },
+      {
+        "type": "Funny",
+        "title": "UX Magazine",
+        "links": "https://uxmag.com/",
+        "description": "Defining and Informing the Complex Field of User Experience (UX)"
+      },
+      {
+        "type": "Novel",
+        "title": "讀創故事",
+        "links": "https://story.udn.com/dcstore/Index.do"
+      }
+    ];
+  export default obj;
+  
